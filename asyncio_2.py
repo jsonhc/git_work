f981e0b0ec86ed1353fd4b991cd1c524bba29604
bob <bob@example.com>
2016-11-14 01:08:11 +0800

    add asyncio_2.py

---
+++ asyncio_2.py
@@ -0,0 +1,16 @@
+import asyncio
+
+@asyncio.coroutine
+def compute(x,y):
+  print('Compute %s + %s ..' % (x,y))
+  yield from asyncio.sleep(1)
+  return x + y
+
+def print_sum(x,y):
+  return = yield from compute(x,y)
+  print('%s + %s = %s' % (x,y,result))
+  
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(print_sum(1,2))
+loop.close()
