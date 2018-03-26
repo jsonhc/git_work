039164cf0420c0d8fa902f6d91739131bb2542d6
wadeson <json_hc@163.com>
2018-03-26 14:41:38 +0800

    Update decorator3.py

--- week3/decorator3.py
+++ week3/decorator3.py
@@ -9,8 +9,9 @@ def dec_student(func):
     return dec
 
 
-@dec_student
+@dec_student   # 将函数student进行装饰，将函数student的函数名作为dec_student函数的参数传入进去
 def student(name, age):
     print(name, age)
 
-student('wadeson', 25)
+student('wadeson', 25)  # 执行该行代码的步骤：1、dec_student(student)('wadeson', 25)    2、return dec---->dec('wadeson', 25)   
+#  3、print("the name in student is %s" % args[0]) and 执行student('wadeson' ,25)
