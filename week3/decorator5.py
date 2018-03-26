0c5f982a7f408aa05ddb6ad6632e0c93b918a99e
wadeson <json_hc@163.com>
2018-03-26 15:01:28 +0800

    Update decorator5.py

--- week3/decorator5.py
+++ week3/decorator5.py
@@ -8,11 +8,12 @@ def auth(auth_type):
     return outer
 
 
-@auth("file")     # auth("file")(langding)()或者auth("file")(langding)(*args, **kwargs)
+@auth("file")     # auth("file")(langing)()或者auth("file")(langing)(*args, **kwargs)
 def langing():
     print("welcome to land")
 
-langing()
+langing()  # 执行该代码的步骤：1、auth('file')(langing)()    2、return outer---->outer(langing)(),由于outer函数传入的参数就是参数名
+# 3、outer(langing)--->return inner---->inner()  4、最后再执行langing函数
 '''
 auth_type is file
 welcome to land
